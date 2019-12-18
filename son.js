8c2aa66617f9a537fea0ffa24918b4fe3820ca38
18384974532 <58537440+18384974532@users.noreply.github.com>
2019-12-18 18:20:25 +0800

    Create son.js

---
+++ son.js
@@ -0,0 +1,41 @@
+var db =openDatabase('mydb4','1.0','order','1024*1024');
+db.transaction(function(contex) {  //建立数据库表单
+contex.executeSql('create table if not exists userinf(name,price,num)');
+});
+
+
+window.onload = function(){
+  db.transaction(function(contex){
+  contex.executeSql('select * from userinf',[],function(con,data){
+  //console.log(data);
+  var rows=data.rows.length,i;
+  console.log(rows);
+  for(var i=0;i<rows;i++){
+    document.getElementById('orderline'+i).innerHTML = (data.rows.item(i).name);
+    document.getElementById('orderline'+i+'c').innerHTML =(data.rows.item(i).num);
+    document.getElementById('orderline'+i+'b').innerHTML =(data.rows.item(i).price);
+  }
+  });
+  });
+}
+
+var oclear = document.getElementById('but1');
+oclear.onclick = function(){
+  db.transaction(function(contex){
+    contex.executeSql('drop table userinf');
+});
+  alert('订单已清空，请刷新页面');
+}
+
+
+db.transaction(function(contex){
+contex.executeSql('select * from userinf',[],function(con,data){
+//console.log(data);
+var rows=data.rows.length,i;
+console.log('总共行数是：'+rows);
+for(var i=0;i<rows;i++){
+console.log(data.rows.item(i).name);
+console.log(data);
+}
+});
+});
